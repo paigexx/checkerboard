59f354fb1a87a863b8a21a75ddbd22b3710544da
paigexx <76425112+paigexx@users.noreply.github.com>
2021-06-10 17:21:24 -0500

    complele

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from logging import debug
 from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("index.html", x=8, y=8, color1 = "red", color2 = "black")
+
+@app.route("/<int:x>")
+def x(x):
+    return render_template("index.html", x=x, y=8, color1 = "red", color2 = "black")
+
+@app.route("/<int:x>/<int:y>")
+def y(x,y):
+    return render_template("index.html", x=x, y=y, color1 = "red", color2 = "black")
+
+@app.route("/<int:x>/<int:y>/<color1>/<color2>")
+def color(x,y,color1,color2):
+    return render_template("index.html", x=x, y=y, color1 = color1, color2 = color2)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
